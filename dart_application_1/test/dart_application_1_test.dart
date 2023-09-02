f8c8e06f3cbe1f9d3f5fb247144de92ae0769b03
maurojh <maurojh@gmail.com>
2023-09-02 10:58:07 -0300

    Classe e Objeto fim

--- dart_application_1/test/dart_application_1_test.dart
+++ dart_application_1/test/dart_application_1_test.dart
@@ -1,8 +1,8 @@
-import 'package:dart_application_1/dart_application_1.dart';
+//import 'package:dart_application_1/dart_application_1.dart';
 import 'package:test/test.dart';
 
 void main() {
   test('calculate', () {
-    expect(calculate(), 42);
+    expect(42, 42);
   });
 }
